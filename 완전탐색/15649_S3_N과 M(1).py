f2d33304bced02a54b57979f5a0731184f5e5560
kiha <rlgktls@naver.com>
2021-07-05 20:52:25 +0900

    0705 문제풀이

---
+++ 완전탐색/15649_S3_N과 M(1).py
@@ -0,0 +1,8 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+from itertools import permutations
+N, M = map(int, input().split())
+P = permutations(range(1, N+1), M)
+for i in P:
+    print(' '.join(map(str, i)))
